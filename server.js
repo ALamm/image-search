a6ab47e583c59ceeb4c1cd27451487b093209229
Alexander L <alamm99@gmail.com>
2016-05-24 19:32:04 +0000

    version 1.0

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
 
-
 var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
@@ -26,25 +25,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.get('/api/latest/imagesearch', function (req,res) {
-	
-// 	var db = mongoose.connection;
-//     db.on('error', console.error.bind(console, 'connection error:'));
-    
-//     var collection = db.collection("history");
-    
-// //    var result = {};
-    
-//     collection.find({}).toArray(function (err,docs) {
-//     if (err) console.error('find error');
-//     res.send(docs);
-//     });
-    
-// });
-
 routes(app, passport);
-
-
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
